aa09b11641504d252ea752372b3dcc8da1d6fe9c
gstar2013 <84575737+gstar2013@users.noreply.github.com>
2022-09-28 12:59:26 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Recipes from './pages/Recepies';
+import SingleRecipe from './pages/SingleRecipe';
+import Default from './pages/Default';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 
 function App() {
+
   return (
-    <div>
-      hello from our recipe app
-    </div>
+    <Router>
+      <main>
+        {/* navbar */}
+        <Navbar />
+      <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/recipes' exact element={<Recipes />} />
+      <Route path='/recipes/:id' element={<SingleRecipe />} />
+      <Route path='*' element={<Default />} />
+
+
+    </Routes>
+    </main>
+    </Router>
+    
   );
-}
+};
+
 
 export default App;
